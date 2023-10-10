886bf59073a2b98e8c71f97c3573bff26693d5e3
Rafael Deluna <delunarafael@yahoo.com>
2023-10-10 02:49:07 -0400

    Completed Sprint 11 Module 3 Web Guided Project

---
+++ my-appo/src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { getTodos, postTodo, putTodo, deleteTodo } from './actions/todos';
+
+
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  
+  const getData = () => {
+      getTodos().then(res => {
+         setTodos(res);
+      })
+  }
+
+  const addTodo = () => {
+    postTodo(todo).then(() => {
+      getData();
+    })
+  }
+
+  const completeTodo = (todo) => {
+     const newTodo = {...todo, isDone: true};
+      putTodo(newTodo).then(() => {
+        getData();
+      })
+  }
+
+  const deleteTodoItem = (id) => {
+    deleteTodo(id).then(() => {
+      getData();
+    })
+  }
+
+
+
+  return (
+    <div className="App">
+    <input value={todo} onChange={(e) => setTodo(e.target.value)}/>
+    <button onClick={() => addTodo()} >Submit</button>
+    {todos.map((todo, index) => (
+      <div key={index}>
+        <span className={todo.isDone ? 'done' : ''}>{todo.description}</span>
+        <span>
+          {todo.isDone ? <button onClick={() => deleteTodoItem(todo.id)}>Delete</button> : <button onClick={() => completeTodo(todo)}>Complete</button>}
+        </span>
+      </div>
+    ))}
+    </div>
+  );
+}
+
+export default App;
